17c58ba2b10ae7c3d48a9f1e5fb2cf8ebb0b9727
Sylvester <sbensons@u.rochester.edu>
2020-12-01 22:02:59 -0500

    Tip Calculator with Javascript

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-//variables
-let billAmt = document.getElementById("bill");
-let gratuity = document.getElementById("gratuity");
-let attendees = document.getElementById("attendees");
+let bill = document.getElementById('bill');
+let gratuity = document.getElementById('gratuity');
+let attendees = document.getElementById('attendees');
 
-//calculate tip
+
 function calculateTip() {
-  console.log(billAmt.value);
-  let total = (billAmt.value * gratuity.value) / attendees.value;
-  total = Math.round(total * 100) / 100;
+  let total = (bill.value  * gratuity.value)/ attendees.value;
+  totatl = Math.round(total * 100)/ 100;
   total = total.toFixed(2);
-  //print
-  document.getElementById("totalTip").style.display = "flex";
-  document.getElementById("tip").innerHTML = `$${total}`;
+
+  document.getElementById('totalTip').style.display = "flex";
+  document.getElementById('tip').innerHTML = `$${total}`;
 }
 
-document.getElementById("calculate").addEventListener("click", calculateTip);
+//Event Listner
+document.getElementById('calculate').addEventListener("click", calculateTip);
